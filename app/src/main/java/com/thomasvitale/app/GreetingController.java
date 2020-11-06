c55f6dd9d6a05afd4956d0abcbdc2629f1508985
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2020-11-06 17:52:21 +0100

    Add step 01 before

---
+++ app/src/main/java/com/thomasvitale/app/GreetingController.java
@@ -0,0 +1,13 @@
+package com.thomasvitale.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@GetMapping("greeting")
+	public String getGreeting() {
+		return "Hello, Devoxx!";
+	}
+}
